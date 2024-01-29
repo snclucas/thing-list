976ca37d5dd70fd17d8f98bafc3abd1be4edc7c8
Simon Clucas <simon.clucas@esa.int>
2024-01-29 20:27:01 +0100

    Added script to add test users

---
+++ admin/add_test_users.py
@@ -0,0 +1,4 @@
+from database_functions import add_user
+
+simon = add_user(username='simon', email='simon@example.com', password='password')
+bob = add_user(username='bob', email='bob@example.com', password='password')
