4f93555d24f4dc427776942a548199ade9b65e4d
Simon Clucas <simon.clucas@esa.int>
2024-02-15 20:08:56 +0100

    Code tidy up

--- utils.py
+++ utils.py
@@ -1,31 +1,49 @@
 from PIL import Image
+from enum import Enum, unique
 
 
-def correct_image_orientation(image: Image):
-    if hasattr(image, '_getexif'):
+@unique
+class ExifOrientation(Enum):
+    ORIENTATION_1 = 1
+    ORIENTATION_2 = 2
+    ORIENTATION_3 = 3
+    ORIENTATION_4 = 4
+    ORIENTATION_5 = 5
+    ORIENTATION_6 = 6
+    ORIENTATION_7 = 7
+    ORIENTATION_8 = 8
+
+
+def correct_image_orientation(image: Image) -> Image:
+    """
+    Corrects the orientation of the image based on the EXIF data.
+
+    Args:
+        image (PIL.Image.Image): The image to correct the orientation of.
+
+    Returns:
+        PIL.Image.Image: The corrected image.
+
+    """
+    orientation = ExifOrientation.ORIENTATION_1  # default value
+
+    if hasattr(image, '_getexif') and callable(image._getexif):
         exifdata = image._getexif()
         try:
-            orientation = exifdata.get(274)
-        except:
-            orientation = 1
-    else:
-        orientation = 1
+            orientation_value = exifdata.get(274)
+            orientation = ExifOrientation(orientation_value)
+        except (AttributeError, ValueError):
+            pass  # Leave orientation as default (1) if can't get the value or it's invalid
 
-    if orientation == 1:
+    if orientation == ExifOrientation.ORIENTATION_1:
         pass
-    elif orientation == 2:
+    elif orientation in {ExifOrientation.ORIENTATION_2}:
         image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    elif orientation == 3:
+    elif orientation in {ExifOrientation.ORIENTATION_3, ExifOrientation.ORIENTATION_4}:
         image = image.rotate(180)
-    elif orientation == 4:
-        image = image.rotate(180)
-    elif orientation == 5:
+    elif orientation in {ExifOrientation.ORIENTATION_5, ExifOrientation.ORIENTATION_6}:
         image = image.rotate(-90)
-    elif orientation == 6:
-        image = image.rotate(-90)
-    elif orientation == 7:
-        image = image.rotate(90)
-    elif orientation == 8:
+    elif orientation in {ExifOrientation.ORIENTATION_7, ExifOrientation.ORIENTATION_8}:
         image = image.rotate(90)
 
     return image
